1100a648ec40eeb2aeea64ece8500bc3f6429b00
hanaikawa <99045513+hanaikawa@users.noreply.github.com>
2022-02-05 20:53:59 +0300

    Update and rename install.sh to i3-setup.sh

--- i3-setup.sh
+++ i3-setup.sh
@@ -1,24 +1,8 @@
-echo Han Aikawa personal Arch Environment script 
-
 echo installing yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
-echo installing i3 environment stage 1
+echo installing i3 environment 
 sudo pacman -S i3-gaps xf86-video-amdgpu xorg ttf-font-awesome easyeffects blueberry rofi
-
 echo installing i3 environment stage 2
 yay -S polybar picom-ibhagwan-git
-
-
-
-
-echo installing pipewire
-sudo pacman -S pipewire-pulse
-
-echo installing Google Chrome
-yay -S google-chrome
-
-reboot
-
