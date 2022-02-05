1601efbf3a1aafe3bfec028513ecb08d86bdf564
hanaikawa <99045513+hanaikawa@users.noreply.github.com>
2022-02-05 14:24:51 +0000

    Update and rename test.sh to install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,13 @@ cd yay
 makepkg -si
 
 echo installing i3 environment stage 1
-sudo pacman -S i3-gaps ttf-font-awesome easyeffects blueberry rofi
+sudo pacman -S i3-gaps xf86-video-amdgpu xorg ttf-font-awesome easyeffects blueberry rofi
 
 echo installing i3 environment stage 2
 yay -S polybar picom-ibhagwan-git
 
-echo installing i3 environment stage 3
-cd $HOME/Downloads
-sudo mv .config $HOME
-
 echo installing LightDM and Greeter
 sudo pacman -S lightdm lightdm-gtk-greeter
-
 echo installing Huawei WMI
 git clone https://github.com/aymanbagabas/Huawei-WMI
 cd Huawei-WMI
@@ -29,29 +24,14 @@ sudo make install
 echo setting autologin
 sudo groupadd -r autologin
 sudo gpasswd -a hanaikawa autologin
-cd $HOME/Downloads
+cd $HOME/Downloads/myconfigs
 sudo cp lightdm.conf /etc/lightdm/lightdm.conf
 
 echo installing pipewire
-sudo pacman -Rns pulse-audio
 sudo pacman -S pipewire-pulse
-
-
-echo installing Steam 
-echo Choose "vulkan-radeon" for AMD, "nvidia-utils" for Nvidia, "vulkan-intel" 
-sudo pacman -S steam
-
-echo installing Discord
-sudo pacman -S discord
-
-echo installing Visual Studio Code
-sudo pacman -S code 
 
 echo installing Google Chrome
 yay -S google-chrome
-
-echo installing Spotify
-yay -S spotify 
 
 reboot
 
