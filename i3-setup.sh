9dea7ecf6cb346a8cb36636d5e352fb1885863a4
hanaikawa <99045513+hanaikawa@users.noreply.github.com>
2022-02-05 20:55:18 +0300

    Update i3-setup.sh

--- i3-setup.sh
+++ i3-setup.sh
@@ -3,6 +3,6 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 echo installing i3 environment 
-sudo pacman -S i3-gaps xf86-video-amdgpu xorg ttf-font-awesome easyeffects blueberry rofi
+sudo pacman -S i3-gaps xf86-video-amdgpu xorg ttf-font-awesome easyeffects blueberry rofi nitrogen
 echo installing i3 environment stage 2
 yay -S polybar picom-ibhagwan-git
